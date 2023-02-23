130cf891169d8dbb382141c4edd832e7126cdc8f
Edel <edelmedina322@gmail.com>
2023-02-23 17:58:43 -0300

    ADd validation to tag controller

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,4 +1,5 @@
 const Tag = require("../models/tag");
+const { body, validationResult } = require("express-validator");
 
 //Info
 exports.tag_list = function (req, res, next) {
@@ -24,19 +25,37 @@ exports.tag_detail = function (req, res, next) {
 };
 
 //Create
-exports.tag_create_post = function (req, res, next) {
-  //Needs to be sanitized
-  const tag = new Tag({
-    name: req.body.name,
-    description: req.body.description,
-  });
-  tag.save((err) => {
-    if (err) {
-      return next(err);
+exports.tag_create_post = [
+  //Fist validates the name field, then checks if genre already exists and if it passes both checks saves new tag
+  body("name", "Tag name required").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const tag = new Tag({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    if (!errors.isEmpty()) {
+      res.json(results);
+    } else {
+      Tag.findOne({ name: req.body.name }).exec((err, found_tag) => {
+        if (err) {
+          return next(err);
+        }
+        if (found_tag) {
+          //Tag already exists
+          res.redirect(found_tag.url);
+        } else {
+          tag.save((err) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(tag.url);
+          });
+        }
+      });
     }
-    res.redirect(tag.url);
-  });
-};
+  },
+];
 
 //Delete
 //Should check if there's cheatsheets assigned and if so not allow the post
@@ -60,7 +79,6 @@ exports.tag_delete_post = function (req, res, next) {
 };
 
 //Update
-//Should check if a tag with the same name already exists
 exports.tag_update_get = function (req, res, next) {
   Tag.findById(req.params.id, function (err, tag_details) {
     if (err) {
@@ -70,18 +88,26 @@ exports.tag_update_get = function (req, res, next) {
     res.json(tag_details);
   });
 };
-exports.tag_update_post = function (req, res, next) {
-  //Needs to be sanitized
-  const tag = new Tag({
-    name: req.body.name,
-    description: req.body.description,
-    _id: req.params.id,
-  });
-  Tag.findByIdAndUpdate(req.params.id, tag, {}, (err, thetag) => {
-    if (err) {
-      return next(err);
+exports.tag_update_post = [
+  //First validates name field, then if there's no errors checks if the tag exists, and if it does updates it and redirects user
+  body("name", "Tag name required").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const tag = new Tag({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+    if (!errors.isEmpty()) {
+      res.json(errors);
+    } else {
+      Tag.findByIdAndUpdate(req.params.id, tag, {}, (err, thetag) => {
+        if (err) {
+          return next(err);
+        }
+        //Successful, so redirect
+        res.redirect(tag.url);
+      });
     }
-    //Successful, so redirect
-    res.redirect(tag.url);
-  });
-};
+  },
+];
